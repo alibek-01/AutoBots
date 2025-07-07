08d88e11adf0e8a5566f58565ecff6db8e5ecd62
Alibek <sadyrkulovvv@gmail.com>
2025-07-07 18:54:08 +0600

    Git classes

--- src/main/java/com/autobots/java/git/Add.java
+++ src/main/java/com/autobots/java/git/Add.java
@@ -2,6 +2,7 @@ package com.autobots.java.git;
 
 public class Add {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        System.out.println("Hi");
     }
 }
